91694a7143a8341a89f35d7747ef63a985a54c67
Kelly Kermode <kkermode@gmail.com>
2022-06-10 11:51:08 -0400

    Update startup.sh

--- script/startup.sh
+++ script/startup.sh
@@ -4,6 +4,8 @@
       sudo yum install -y httpd mariadb-server
       sudo systemctl start httpd
       sudo systemctl enable httpd
+      # allows ec2-user to access the web server
       sudo usermod -a -G apache ec2-user
+      # assigns the web server to the ec2-user
       sudo chown -R ec2-user:apache /var/www
       sudo echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
